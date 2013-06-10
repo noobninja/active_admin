a425648409de59edb99bcd04990c2cfa248ece1a
noobninja <jamil@noobninja.com>
2013-06-10 20:18:27 -0300

    Update application.rb

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -247,17 +247,9 @@ module ActiveAdmin
     def attach_reloader
       ActiveAdmin::Reloader.build(Rails.application, self, Rails.version).attach!
     end
-
-    def generate_stylesheets
-      # Create our own asset pipeline in Rails 3.0
-      if ActiveAdmin.use_asset_pipeline?
-        # Add our mixins to the load path for SASS
-        ::Sass::Engine::DEFAULT_OPTIONS[:load_paths] <<  File.expand_path("../../../app/assets/stylesheets", __FILE__)
-      else
-        require 'active_admin/sass/css_loader'
-        ::Sass::Plugin.add_template_location(File.expand_path("../../../app/assets/stylesheets", __FILE__))
-        ::Sass::Plugin.add_template_location(File.expand_path("../sass", __FILE__))
-      end
+    
+    if ActiveAdmin.use_asset_pipeline?
+      ::Sass::Engine::DEFAULT_OPTIONS[:load_paths] << File.expand_path("../../../app/assets/stylesheets", __FILE__)
     end
   end
 end
